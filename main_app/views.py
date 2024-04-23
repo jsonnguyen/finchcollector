09288dd1785c2567cdcd504c265540d0ef8dc4d5
Jason Nguyen <j1.nguyen@yahoo.com>
2024-04-23 14:54:23 -0700

    complete part 4

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Finch
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
 
 def home(request):
     return render(request, 'home.html')
@@ -16,9 +17,19 @@ def finches_index(request):
 
 def finch_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
-        'finch': finch
+        'finch': finch,
+        'feeding_form': feeding_form
     })
+
+def add_feeding(request, finch_id):
+    submitted_form = FeedingForm(request.POST)
+    if submitted_form.is_valid():
+        new_feeding = submitted_form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
     model = Finch
